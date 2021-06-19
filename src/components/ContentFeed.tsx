5c794794944e38197135523748383f5607828579
Connor Dean <connor.timothy.dean@gmail.com>
2021-06-19 17:39:04 -0500

    CD: Move ScrollView to FlatList and handle pagination.

--- src/components/ContentFeed.tsx
+++ src/components/ContentFeed.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { ScrollView } from "react-native-gesture-handler";
+import { FlatList } from "react-native";
 import { makeAPIRequest, APIEndpoints } from "../utils/networkRequest";
 import FeedCard from "./FeedCard";
 
@@ -38,66 +38,72 @@ type ContentFeedProps = {
   contentType: APIEndpoints.ARTICLES | APIEndpoints.VIDEOS
 }
 
+const FETCH_QUANTITY = 20
+
 const ContentFeed: React.FC<ContentFeedProps> = ({ contentType }) => {
   const [contentItems, setContentItems] = useState<ContentFeedDTO[]>([])
+  const [queryStartIndex, setQuertStartIndex] = useState<number>(0)
+
+  const getCommentCounts = async (contentIds: string[]): Promise<CommentCount[]> => {
+    let queryString = "?ids="
+    for (const contentId of contentIds) {
+      queryString += `${contentId},`
+    }
+
+    const comments = await makeAPIRequest(APIEndpoints.COMMENT_COUNT, "GET", queryString)
+    return comments.content
+  }
+
+  const getContent = async () => {
+    const queryString = `?startIndex=${queryStartIndex}&count=${FETCH_QUANTITY}`
+
+    const contentResponse = await makeAPIRequest(contentType, "GET", queryString)
+
+    const contentIds = contentResponse.data.map((content: any) => content.contentId)
+    const commentCounts: CommentCount[] = await getCommentCounts(contentIds)
+
+    const mappedContent: ContentFeedDTO[] = contentResponse.data.map((content: any) => {
+      const comment = commentCounts.find((comment) => content.contentId === comment.id)
+
+      /**
+       * Questions:
+       * - Would the API always return these thumbnails in this order or would they have to be manually mapped?
+       */
+
+      return {
+        id: content.contentId,
+        contentType: content.contentType,
+        headline: content.metadata.headline,
+        smallThumbnail: content.thumbnails[0],
+        mediumThumbnail: content.thumbnails[1],
+        largeThumbnail: content.thumbnails[2],
+        description: content.metadata.description,
+        ...(content.authors && { authors: content.authors }),
+        networks: content.metadata.networks.length ? content.metadata.networks : [],
+        commentCount: comment?.count,
+        publishDate: content.metadata.publishDate
+      }
+    })
+
+    setContentItems((prevMappedContent) => [...prevMappedContent, ...mappedContent])
+  }
 
   useEffect(() => {
-    const getCommentCounts = async (contentIds: string[]): Promise<CommentCount[]> => {
-      let queryString = "?ids="
-      for (const contentId of contentIds) {
-        queryString += `${contentId},`
-      }
-
-      const comments = await makeAPIRequest(APIEndpoints.COMMENT_COUNT, "GET", queryString)
-      return comments.content
-    }
-
-    const getContent = async () => {
-      const contentResponse = await makeAPIRequest(contentType, "GET")
-
-      const contentIds = contentResponse.data.map((content: any) => content.contentId)
-      const commentCounts: CommentCount[] = await getCommentCounts(contentIds)
-
-      const mappedArticles: ContentFeedDTO[] = contentResponse.data.map((content: any) => {
-        const comment = commentCounts.find((comment) => content.contentId === comment.id)
-
-        /**
-         * Questions:
-         * - Would the API always return these thumbnails in this order or would they have to be manually mapped?
-         */
-
-        return {
-          id: content.contentId,
-          contentType: content.contentType,
-          headline: content.metadata.headline,
-          smallThumbnail: content.thumbnails[0],
-          mediumThumbnail: content.thumbnails[1],
-          largeThumbnail: content.thumbnails[2],
-          description: content.metadata.description,
-          ...(content.authors && { authors: content.authors }),
-          networks: content.metadata.networks.length ? content.metadata.networks : [],
-          commentCount: comment?.count,
-          publishDate: content.metadata.publishDate
-        }
-      })
-
-      setContentItems(mappedArticles)
-    }
-
     getContent()
-  }, [])
+  }, [queryStartIndex])
+
+  const renderCard = (contentItem: ContentFeedDTO): JSX.Element => (
+    <FeedCard content={contentItem} />
+  )
 
   return (
-    <ScrollView>
-      {contentItems.length ? (
-        contentItems.map((content: ContentFeedDTO) => {
-          return (
-            <FeedCard content={content} key={content.id} />
-          )
-        })
-      ): null}
-    </ScrollView>
-  );
+    <FlatList
+      data={contentItems}
+      renderItem={({ item }) => renderCard(item)}
+      onEndReachedThreshold={0.75}
+      onEndReached={() => setQuertStartIndex(queryStartIndex + FETCH_QUANTITY)}
+    />
+  )
 };
 
 export default ContentFeed;
